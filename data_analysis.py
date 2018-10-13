d60fa790e9af1313c0f1c465bb51ebf7b173cef1
Ze Canelha <zecanelha@gmail.com>
2018-10-13 23:41:04 +0100

    Reading dataset

---
+++ data_analysis.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd 
+
+path1 = "/Users/zecanelha/Desktop/1o Semestre/MEI/Datasets/SDSC-SP2.txt"
+
+path2 = "MEI/Datasets/HPC2N.txt"
+
+columns = ["Job Number", "Submit Time","Wait Time","Run Time","Number of Allocated Processors","Average CPU Time Used","Used Memory," \
+			"Requested Number of Processors","Requested Time","Requested Memory","Status","UserID","Group ID","Executable Number", \
+           	"Queue Number","Partition Number","Preceding Job Number","Think"]
+
+try:
+	dataset = pd.read_table(path1, delimiter = ',', header = None, names = columns);
+except IOError as e:
+	print(e)
+
+print(dataset)
+
