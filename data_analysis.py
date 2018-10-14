23d5be06364f2b9561dd79605132905bc031c5d4
Ze Canelha <zecanelha@gmail.com>
2018-10-14 20:10:08 +0100

    Timezone and minor bug

--- data_analysis.py
+++ data_analysis.py
@@ -1,22 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd 
+import pandas as pd
+from datetime import datetime
+import time, os
+
+
+# Notes:
+# SDSC-SP2
+# Duration:	May 1998 thru April 2000
+# TimeZoneString is a standard UNIX string indicating the time zone in which the log was generated
+# UnixStartTime: 893466664
+# TimeZone: -28800
+# TimeZoneString: US/Pacific
+# StartTime: Fri Apr 24 18:11:04 PDT 1998
+# EndTime:   Sat Apr 29 21:08:32 PDT 2000
+
+UnixStartTime = 893466664
 
 path1 = "/Users/zecanelha/Desktop/1o Semestre/MEI/Datasets/SDSC-SP2.txt"
 
 path2 = "MEI/Datasets/HPC2N.txt"
 
-columns = ["Job Number", "Submit Time","Wait Time","Run Time","Number of Allocated Processors","Average CPU Time Used","Used Memory," \
+columns = ["Job Number", "Submit Time","Wait Time","Run Time","Number of Allocated Processors","Average CPU Time Used","Used Memory", \
 			"Requested Number of Processors","Requested Time","Requested Memory","Status","UserID","Group ID","Executable Number", \
            	"Queue Number","Partition Number","Preceding Job Number","Think"]
 
+
 try:
-	dataset = pd.read_table(path1, delimiter = ',', header = None, names = columns);
+	dataset = pd.read_table(path1, delimiter = ',', header = None, names = columns, index_col = False);
 except IOError as e:
 	print(e)
 
+os.environ['TZ'] = 'US/Pacific'
+time.tzset()
+# print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(UnixStartTime)))
+# logStartTime = dataset.loc[dataset.index[0],'Submit Time']
 
-# Get number of canceled jobs - 5 - and finished jobs - 1 - 
+
+
+# Get number of canceled jobs - 5 - and finished jobs - 1 -
 
 status = dataset['Status']
 plt.hist(status, bins = 'auto', align = 'mid', label = 'Status of the jobs')
@@ -26,7 +48,7 @@ plt.legend()
 plt.show()
 
 
-# Get number of jobs per user 
+# Get number of jobs per user
 
 #Get unique users -> set unordered colection of distinct objects
 
@@ -48,7 +70,3 @@ plt.show()
 
 
 # Get Number of complete jobs per weak
-
-
-
-
